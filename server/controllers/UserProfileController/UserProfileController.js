ab213cb9434e6c70980ee288fd42bf83e8cd451f
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-05-02 15:31:06 +0530

    user profile updated

--- server/controllers/UserProfileController/UserProfileController.js
+++ server/controllers/UserProfileController/UserProfileController.js
@@ -1,7 +1,19 @@
-const {UserProfileModel} = require("../../models")
+const {UserProfileModel,Auth} = require("../../models")
 
 const UserProfileController = async(req,res)=>{
   try {
+    const currentUserId = req.body.currentUserId
+
+    console.log(currentUserId);
+    
+    const userData = await Auth.findOne({where:{ googleId:currentUserId }})
+
+    res.status(200).send({
+      success:true,
+      message:"User Profile",
+      userData : userData
+    })
+
         
   } catch (error) {
       res.status(400).send({
