7314ba8760e8099a3db1f0935f2fff59f8ac1853
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-05-11 20:06:53 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "http://localhost:3001",
       methods: ["GET", "POST"],
     },
   });
