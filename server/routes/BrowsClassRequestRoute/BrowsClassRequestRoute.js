1f15219543a272352f0afeb78fa4980c46d323dc
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-06-16 18:35:01 +0530

    brows class request modify.

--- server/routes/BrowsClassRequestRoute/BrowsClassRequestRoute.js
+++ server/routes/BrowsClassRequestRoute/BrowsClassRequestRoute.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const { BrowsClassRequestController, FilterClassRequestController } = require("../../controllers/BrowsClassRequestController/BrowsClassRequestController");
+const { BrowsClassRequestController, FilterClassRequestController, BrowsClassRequestPaginationController } = require("../../controllers/BrowsClassRequestController/BrowsClassRequestController");
 const router = express.Router();
 
 // FILTER DATA || POST
 router.post("/data",BrowsClassRequestController)
 
+
 // FILTER DATA || POST || USING SUBJECT AND LOCATION
 router.post("/filter_data",FilterClassRequestController)
 
+
+
+// PAGINATIONS || POST
+router.post("/Requestpagination",BrowsClassRequestPaginationController)
 
 module.exports = router;
