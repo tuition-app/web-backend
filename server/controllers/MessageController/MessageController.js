6479dc3290fc0c4cd35a7cc0e9e3a85de11b2c46
Imesh Thanapathi <imeshthanapathi@gmail.com>
2024-07-05 09:16:19 +0530

    Changes in MessageController and MessageModel

--- server/controllers/MessageController/MessageController.js
+++ server/controllers/MessageController/MessageController.js
@@ -1,74 +1,68 @@
-const {MessageModel} = require("../../models")
+const moment = require('moment-timezone');
+const { MessageModel } = require("../../models");
 
+const generateRoomId = (id1, id2) => {
+    return [id1, id2].sort().join('_');
+};
 
-const MessageController = async(req,res)=>{
+const MessageController = async (req, res) => {
   try {
-        console.log(req.body);
-     const {senderId , receiverId , message} = req.body
+      const { senderId, receiverId, message } = req.body;
+      const roomId = generateRoomId(senderId, receiverId);
+      const time = moment().tz('Asia/Kolkata').toISOString();
 
-     const newMessage = new MessageModel({
-          senderId:senderId,
-          receiverId:receiverId,
-          message:message
-     })
+      const newMessage = new MessageModel({
+          senderId: senderId,
+          receiverId: receiverId,
+          message: message,
+          roomId: roomId,
+          time : time
+      });
 
-     await newMessage.save()
+      await newMessage.save();
 
-     res.status(200).send({
-        success:true,
-        message:"Message store successfull",
-        // newMessage:newMessage
-      })
-
+      res.status(200).send({
+          success: true,
+          message: "Message stored successfully",
+      });
   } catch (error) {
-       res.status(400).send({
-         success:false,
-         message:"Error in MessageController",
-         error
-       })  
+      res.status(400).send({
+          success: false,
+          message: "Error in MessageController",
+          error
+      });
   }
-}
+};
 
 
-const MessageDataController = async(req,res)=>{
-     try {
-        console.log(req.body);
-        const currentUserId = req.body.currentUserId
-        const matchingUserId = req.body.matchingUserId
-        const receive_messages = []
-        const matchingMessages = []
+const MessageDataController = async (req, res) => {
+    try {
+        const { currentUserId, matchingUserId } = req.body;
+        const roomId = generateRoomId(currentUserId, matchingUserId);
 
-        const messageData = await MessageModel.findAll({})
-
-        for (let i = 0; i < messageData.length; i++) {
-          if(messageData[i].receiverId === currentUserId){
-              receive_messages.push(messageData[i])
-          }
-          
-        }
-
-        for (let j = 0; j < receive_messages.length; j++) {
-          if (receive_messages[j].senderId === matchingUserId) {
-            matchingMessages.push(receive_messages[j])
-          }
-          
-        }
+        const messageData = await MessageModel.findAll({
+            where: {
+                roomId: roomId
+            }
+        });
 
         res.status(200).send({
-            success:true,
-            message:"Message Data Fetch Successfull",
-            receive_messages,
-            matchingMessages
-        })
+          success: true,
+          message: "Message Data Fetch Successful",
+          messageData: messageData.map(msg => ({
+              senderId: msg.senderId.toString(),
+              receiverId: msg.receiverId.toString(),
+              message: msg.message,
+              time: moment(msg.time).tz('Asia/Kolkata').toISOString()
+          }))
+        });
+    } catch (error) {
+        res.status(400).send({
+            success: false,
+            message: "Error in MessageDataController",
+            error
+        });
+    }
+};
 
-
-     } catch (error) {
-         res.status(400).send({
-             success:false,
-             message:"Error in MessageDataController",
-             error
-         })
-     }   
-}
-
-module.exports = {MessageController,MessageDataController}
+module.exports = { MessageController, MessageDataController };
--- server/models/MessageModel.js
+++ server/models/MessageModel.js
@@ -1,31 +1,40 @@
-// MessageModel
 
-module.exports = (Sequelize, DataTypes)=>{
+module.exports = (Sequelize, DataTypes) => {
+    const MessageModel = Sequelize.define("MessageModel", {
+        senderId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                notEmpty: true
+            }
+        },
+        receiverId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                notEmpty: true
+            }
+        },
+        message: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                notEmpty: true
+            }
+        },
+        roomId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                notEmpty: true
+            }
+        },
+        time: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            // defaultValue: Sequelize.NOW
+        }
+    });
 
-        const MessageModel = Sequelize.define("MessageModel",{
-            senderId:{
-                type: DataTypes.STRING,
-                allowNull: false,
-                validation:{
-                   notEmpty: true
-                }
-            },
-            receiverId:{
-                type: DataTypes.STRING,
-                allowNull: false,
-                validation:{
-                   notEmpty: true
-                }
-            },
-            message:{
-                type: DataTypes.STRING,
-                allowNull: false,
-                validation:{
-                   notEmpty: true
-                }
-            },        
-        })
-    
-        return MessageModel;
-     
-     }
+    return MessageModel;
+};
